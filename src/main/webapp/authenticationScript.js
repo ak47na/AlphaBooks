89fc05223b3ce0575d7b0c0f10dcb84b280a1af7
AnaK <ana.kapros@yahoo.ro>
2020-08-11 11:27:08 +0000

    add authenticationScript to handle user authentication using Email, Google or Facebook accounts

---
+++ src/main/webapp/authenticationScript.js
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @return {!Object} The FirebaseUI config.
+ */
+function getUIConfig() {
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        if (authResult.user) {
+          handleSignedInUser(authResult.user);
+        }
+        if (authResult.additionalUserInfo) {
+          document.getElementById('is-new-user').textContent =
+              authResult.additionalUserInfo.isNewUser ?
+              'New User' : 'Existing User';
+        }
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+    //the sign-in providers you offered for users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+}
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// Disable auto-sign in.
+ui.disableAutoSignIn();
+
+/**
+ * @return {string} The URL of the FirebaseUI standalone widget.
+ */
+function getWidgetUrl() {
+  return '/widget#recaptcha=' + getRecaptchaMode() + '&emailSignInMethod=' +
+      getEmailSignInMethod();
+}
+
+/**
+ * Redirects to the FirebaseUI widget.
+ */
+var signInWithRedirect = function() {
+  window.location.assign(getWidgetUrl());
+};
+
+/**
+ * Open a popup with the FirebaseUI widget.
+ */
+var signInWithPopup = function() {
+  window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
+};
+
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+  document.getElementById('user-signed-in').style.display = 'block';
+  document.getElementById('user-signed-out').style.display = 'none';
+  document.getElementById('name').textContent = user.displayName;
+  document.getElementById('email').textContent = user.email;
+};
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'block';
+  ui.start('#firebaseui-container', getUiConfig());
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('loaded').style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+/**
+ * Handles when the user changes the reCAPTCHA or email signInMethod config.
+ */
+function handleConfigChange() {
+  var newRecaptchaValue = document.querySelector(
+      'input[name="recaptcha"]:checked').value;
+  var newEmailSignInMethodValue = document.querySelector(
+      'input[name="emailSignInMethod"]:checked').value;
+  location.replace(
+      location.pathname + '#recaptcha=' + newRecaptchaValue +
+      '&emailSignInMethod=' + newEmailSignInMethodValue);
+
+  // Reset the inline widget so the config changes are reflected.
+  ui.reset();
+  ui.start('#firebaseui-container', getUiConfig());
+}
+
+/**
+ * Initializes the app.
+ */
+var initApp = function() {
+  document.getElementById('sign-in-with-redirect').addEventListener(
+      'click', signInWithRedirect);
+  document.getElementById('sign-in-with-popup').addEventListener(
+      'click', signInWithPopup);
+  document.getElementById('sign-out').addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+
+  document.getElementById('recaptcha-normal').addEventListener(
+      'change', handleConfigChange);
+  document.getElementById('recaptcha-invisible').addEventListener(
+      'change', handleConfigChange);
+  // Check the selected reCAPTCHA mode.
+  document.querySelector(
+      'input[name="recaptcha"][value="' + getRecaptchaMode() + '"]')
+      .checked = true;
+
+  document.getElementById('email-signInMethod-password').addEventListener(
+      'change', handleConfigChange);
+  document.getElementById('email-signInMethod-emailLink').addEventListener(
+      'change', handleConfigChange);
+  // Check the selected email signInMethod mode.
+  document.querySelector(
+      'input[name="emailSignInMethod"][value="' + getEmailSignInMethod() + '"]')
+      .checked = true;
+};
+
+window.addEventListener('load', initApp);
